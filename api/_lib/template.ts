41a033f700b81e84382523dd0109523874691c8c
Quentin <q.claes@bridgewater.be>
2020-09-15 16:58:43 +0200

    Update template.ts

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -110,7 +110,37 @@ export function getHtml(parsedReq: ParsedRequest) {
     heights,
     rating,
   } = parsedReq;
-
+  getData();
+  return `<!DOCTYPE html>
+<html>
+    <meta charset="utf-8">
+    <title>Generated Image</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        ${getCss(theme, fontSize)}
+    </style>
+    <body>
+        <div>
+            <div class="spacer">
+            <h2>www.cuustomer.com  ${rating}</h2>
+            <div class="logo-wrapper">
+                ${images
+                  .map(
+                    (img, i) =>
+                      getPlusSign(i) + getImage(img, widths[i], heights[i])
+                  )
+                  .join("")}
+            </div>
+            <div class="spacer">
+            <div class="heading">${emojify(
+              md ? marked(text) : sanitizeHtml(text)
+            )}
+            </div>
+        </div>
+    </body>
+</html>`;
+}
+function getData() {
   axios({
     url: "https://cuustomer-api-cafdaa7625.herokuapp.com/cuustomer-new-api/dev",
     method: "post",
@@ -128,37 +158,8 @@ export function getHtml(parsedReq: ParsedRequest) {
     },
   }).then((result) => {
     console.log(result.data);
-    return `<!DOCTYPE html>
-    <html>
-        <meta charset="utf-8">
-        <title>Generated Image</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style>
-            ${getCss(theme, fontSize)}
-        </style>
-        <body>
-            <div>
-                <div class="spacer">
-                <h2>www.cuustomer.com  ${rating}</h2>
-                <div class="logo-wrapper">
-                    ${images
-                      .map(
-                        (img, i) =>
-                          getPlusSign(i) + getImage(img, widths[i], heights[i])
-                      )
-                      .join("")}
-                </div>
-                <div class="spacer">
-                <div class="heading">${emojify(
-                  md ? marked(text) : sanitizeHtml(text)
-                )}
-                </div>
-            </div>
-        </body>
-    </html>`;
   });
 }
-
 function getImage(src: string, width = "auto", height = "225") {
   return `<img
         class="logo"
