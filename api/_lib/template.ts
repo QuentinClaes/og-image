a1604858727c5f06e3e1a81b3e707cf7fb3c6514
Quentin <q.claes@bridgewater.be>
2020-09-15 16:12:10 +0200

    Update template.ts

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -1,114 +1,114 @@
-import { readFileSync } from "fs";
-import marked from "marked";
-import { sanitizeHtml } from "./sanitizer";
-import { ParsedRequest } from "./types";
+// import { readFileSync } from "fs";
+// import marked from "marked";
+// import { sanitizeHtml } from "./sanitizer";
+// import { ParsedRequest } from "./types";
 import axios from "axios";
-const twemoji = require("twemoji");
-const twOptions = { folder: "svg", ext: ".svg" };
-const emojify = (text: string) => twemoji.parse(text, twOptions);
+// const twemoji = require("twemoji");
+// const twOptions = { folder: "svg", ext: ".svg" };
+// const emojify = (text: string) => twemoji.parse(text, twOptions);
 
-const rglr = readFileSync(
-  `${__dirname}/../_fonts/Inter-Regular.woff2`
-).toString("base64");
-const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString(
-  "base64"
-);
-const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString(
-  "base64"
-);
+// const rglr = readFileSync(
+//   `${__dirname}/../_fonts/Inter-Regular.woff2`
+// ).toString("base64");
+// const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString(
+//   "base64"
+// );
+// const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString(
+//   "base64"
+// );
 
-function getCss(theme: string, fontSize: string) {
-  let background = "white";
-  let foreground = "black";
-  let radial = "lightgray";
+// function getCss(theme: string, fontSize: string) {
+//   let background = "white";
+//   let foreground = "black";
+//   let radial = "lightgray";
 
-  if (theme === "dark") {
-    background = "black";
-    foreground = "white";
-    radial = "dimgray";
-  }
-  return `
-    @font-face {
-        font-family: 'Inter';
-        font-style:  normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
-    }
+//   if (theme === "dark") {
+//     background = "black";
+//     foreground = "white";
+//     radial = "dimgray";
+//   }
+//   return `
+//     @font-face {
+//         font-family: 'Inter';
+//         font-style:  normal;
+//         font-weight: normal;
+//         src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
+//     }
 
-    @font-face {
-        font-family: 'Inter';
-        font-style:  normal;
-        font-weight: bold;
-        src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
-    }
+//     @font-face {
+//         font-family: 'Inter';
+//         font-style:  normal;
+//         font-weight: bold;
+//         src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
+//     }
 
-    @font-face {
-        font-family: 'Vera';
-        font-style: normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${mono})  format("woff2");
-      }
+//     @font-face {
+//         font-family: 'Vera';
+//         font-style: normal;
+//         font-weight: normal;
+//         src: url(data:font/woff2;charset=utf-8;base64,${mono})  format("woff2");
+//       }
 
-    body {
-        background: ${background};
-        background-image: radial-gradient(circle at 25px 25px, ${radial} 2%, transparent 0%), radial-gradient(circle at 75px 75px, ${radial} 2%, transparent 0%);
-        background-size: 100px 100px;
-        height: 627px;
-        width: 1200px;
-        display: flex;
-        text-align: center;
-        align-items: center;
-        justify-content: center;
-    }
+//     body {
+//         background: ${background};
+//         background-image: radial-gradient(circle at 25px 25px, ${radial} 2%, transparent 0%), radial-gradient(circle at 75px 75px, ${radial} 2%, transparent 0%);
+//         background-size: 100px 100px;
+//         height: 627px;
+//         width: 1200px;
+//         display: flex;
+//         text-align: center;
+//         align-items: center;
+//         justify-content: center;
+//     }
 
-    code {
-        color: #D400FF;
-        font-family: 'Vera';
-        white-space: pre-wrap;
-        letter-spacing: -5px;
-    }
+//     code {
+//         color: #D400FF;
+//         font-family: 'Vera';
+//         white-space: pre-wrap;
+//         letter-spacing: -5px;
+//     }
 
-    code:before, code:after {
-        content: '\`';
-    }
+//     code:before, code:after {
+//         content: '\`';
+//     }
 
-    .logo-wrapper {
-        display: flex;
-        align-items: center;
-        align-content: center;
-        justify-content: center;
-        justify-items: center;
-    }
+//     .logo-wrapper {
+//         display: flex;
+//         align-items: center;
+//         align-content: center;
+//         justify-content: center;
+//         justify-items: center;
+//     }
 
-    .logo {
-        margin: 0 75px;
-    }
+//     .logo {
+//         margin: 0 75px;
+//     }
 
-    .plus {
-        color: #BBB;
-        font-family: Times New Roman, Verdana;
-        font-size: 100px;
-    }
+//     .plus {
+//         color: #BBB;
+//         font-family: Times New Roman, Verdana;
+//         font-size: 100px;
+//     }
 
-    .spacer {
-        margin: 150px;
-    }
+//     .spacer {
+//         margin: 150px;
+//     }
 
-    .emoji {
-        height: 1em;
-        width: 1em;
-        margin: 0 .05em 0 .1em;
-        vertical-align: -0.1em;
-    }
+//     .emoji {
+//         height: 1em;
+//         width: 1em;
+//         margin: 0 .05em 0 .1em;
+//         vertical-align: -0.1em;
+//     }
 
-    .heading {
-        font-family: 'Inter', sans-serif;
-        font-size: ${sanitizeHtml(fontSize)};
-        font-style: normal;
-        color: ${foreground};
-        line-height: 1.8;
-    }`;
-}
+//     .heading {
+//         font-family: 'Inter', sans-serif;
+//         font-size: ${sanitizeHtml(fontSize)};
+//         font-style: normal;
+//         color: ${foreground};
+//         line-height: 1.8;
+//     }`;
+// }
 
 // export function getHtml(parsedReq: ParsedRequest) {
 // const {
