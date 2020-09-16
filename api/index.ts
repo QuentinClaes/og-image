26e4bf673b952c0b29420c5ee00471f5cd0df8ba
quentinclaes6@gmail.com <quentinclaes6@gmail.com>
2020-09-16 14:01:27 +0200

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -18,7 +18,7 @@ function getData(parsedReq: ParsedRequest) {
     },
     data: {
       query: `query {
-          reviews(where:{provider:{name: "${text}"}, id: ${id}}){
+          reviews(where:{provider:{name: "${text ? text : "Proximus"}"}, id: ${id}}){
             id
             title
             content
