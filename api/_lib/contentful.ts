4ff258a65a163c5f82c5155b7ea63401d0645090
Quentin Claes <quentin@cuustomer.com>
2020-09-18 15:22:26 +0200

    Update contentful.ts

--- api/_lib/contentful.ts
+++ api/_lib/contentful.ts
@@ -16,7 +16,7 @@ export async function getImage(ImageLabel: string) {
       const test = client
         .getAsset(response.items[0].sys.id)
         .then((asset: any) => {
-            var url = `https:${asset.fields.file.url}?fit=pad&fl=progressive&fm=jpg&w=2OO&h=200`
+            var url = `https:${asset.fields.file.url}?fm=jpg&w=2OO&h=200`
             
             return  url 
             }   
